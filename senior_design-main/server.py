64fe552d88b84b630f0040fce3c1760b7e3008d0
Vishall Manikaran <manik048@umn.edu>
2023-10-05 14:36:45 -0500

    Dubins Path

--- senior_design-main/server.py
+++ senior_design-main/server.py
@@ -3,6 +3,12 @@ import cv2
 import sys
 import socket
 import pickle
+import sys
+import pathlib
+sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
+
+from math import sin, cos, atan2, sqrt, acos, pi, hypot
+from utils.angle import angle_mod, rot_mat_2d
 
 UDP_PORT = 1234
 
@@ -40,7 +46,97 @@ class Main():
         cv2.setMouseCallback("frame", self.handle_mouse)
 
     # Vishall
-    def compute_dubins_path(self):
+    def _mod2pi(theta):
+        return angle_mod(theta, zero_2_2pi=True)
+    
+    def calc_trig_funcs(alpha, beta):
+        sin_a = sin(alpha)
+        sin_b = sin(beta)
+        cos_a = cos(alpha)
+        cos_b = cos(beta)
+        cos_ab = cos(alpha - beta)
+        return sin_a, sin_b, cos_a, cos_b, cos_ab
+
+    def _LSL(alpha, beta, d):
+        sin_a, sin_b, cos_a, cos_b, cos_ab = calc_trig_funcs(alpha, beta)
+        mode = ["L", "S", "L"]
+        p_squared = 2 + d ** 2 - (2 * cos_ab) + (2 * d * (sin_a - sin_b))
+        if p_squared < 0:  # invalid configuration
+            return None, None, None, mode
+        tmp = atan2((cos_b - cos_a), d + sin_a - sin_b)
+        d1 = _mod2pi(-alpha + tmp)
+        d2 = sqrt(p_squared)
+        d3 = _mod2pi(beta - tmp)
+        return d1, d2, d3, mode
+
+
+    def _RSR(alpha, beta, d):
+        sin_a, sin_b, cos_a, cos_b, cos_ab = calc_trig_funcs(alpha, beta)
+        mode = ["R", "S", "R"]
+        p_squared = 2 + d ** 2 - (2 * cos_ab) + (2 * d * (sin_b - sin_a))
+        if p_squared < 0:
+            return None, None, None, mode
+        tmp = atan2((cos_a - cos_b), d - sin_a + sin_b)
+        d1 = _mod2pi(alpha - tmp)
+        d2 = sqrt(p_squared)
+        d3 = _mod2pi(-beta + tmp)
+        return d1, d2, d3, mode
+
+
+    def _LSR(alpha, beta, d):
+        sin_a, sin_b, cos_a, cos_b, cos_ab = calc_trig_funcs(alpha, beta)
+        p_squared = -2 + d ** 2 + (2 * cos_ab) + (2 * d * (sin_a + sin_b))
+        mode = ["L", "S", "R"]
+        if p_squared < 0:
+            return None, None, None, mode
+        d1 = sqrt(p_squared)
+        tmp = atan2((-cos_a - cos_b), (d + sin_a + sin_b)) - atan2(-2.0, d1)
+        d2 = _mod2pi(-alpha + tmp)
+        d3 = _mod2pi(-_mod2pi(beta) + tmp)
+        return d2, d1, d3, mode
+
+
+    def _RSL(alpha, beta, d):
+        sin_a, sin_b, cos_a, cos_b, cos_ab = calc_trig_funcs(alpha, beta)
+        p_squared = d ** 2 - 2 + (2 * cos_ab) - (2 * d * (sin_a + sin_b))
+        mode = ["R", "S", "L"]
+        if p_squared < 0:
+            return None, None, None, mode
+        d1 = sqrt(p_squared)
+        tmp = atan2((cos_a + cos_b), (d - sin_a - sin_b)) - atan2(2.0, d1)
+        d2 = _mod2pi(alpha - tmp)
+        d3 = _mod2pi(beta - tmp)
+        return d2, d1, d3, mode
+
+
+    def _RLR(alpha, beta, d):
+        sin_a, sin_b, cos_a, cos_b, cos_ab = calc_trig_funcs(alpha, beta)
+        mode = ["R", "L", "R"]
+        tmp = (6.0 - d ** 2 + 2.0 * cos_ab + 2.0 * d * (sin_a - sin_b)) / 8.0
+        if abs(tmp) > 1.0:
+            return None, None, None, mode
+        d2 = _mod2pi(2 * pi - acos(tmp))
+        d1 = _mod2pi(alpha - atan2(cos_a - cos_b, d - sin_a + sin_b) + d2 / 2.0)
+        d3 = _mod2pi(alpha - beta - d1 + d2)
+        return d1, d2, d3, mode
+
+
+    def _LRL(alpha, beta, d):
+        sin_a, sin_b, cos_a, cos_b, cos_ab = calc_trig_funcs(alpha, beta)
+        mode = ["L", "R", "L"]
+        tmp = (6.0 - d ** 2 + 2.0 * cos_ab + 2.0 * d * (- sin_a + sin_b)) / 8.0
+        if abs(tmp) > 1.0:
+            return None, None, None, mode
+        d2 = _mod2pi(2 * pi - acos(tmp))
+        d1 = _mod2pi(-alpha - atan2(cos_a - cos_b, d + sin_a - sin_b) + d2 / 2.0)
+        d3 = _mod2pi(_mod2pi(beta) - alpha - d1 + _mod2pi(d2))
+        return d1, d2, d3, mode
+    
+    _PATH_TYPE_MAP = {"LSL": _LSL, "RSR": _RSR, "LSR": _LSR, "RSL": _RSL,
+                  "RLR": _RLR, "LRL": _LRL, }
+    
+    def compute_dubins_path(s_x, s_y, s_yaw, g_x, g_y, g_yaw, curvature,
+                     step_size=0.1, selected_types=None):
         # https://atsushisakai.github.io/PythonRobotics/modules/path_planning/dubins_path/dubins_path.html
         # https://github.com/AtsushiSakai/PythonRobotics/blob/master/PathPlanning/DubinsPath/dubins_path_planner.py
 
@@ -50,8 +146,36 @@ class Main():
         # determine length of the each segment in the shortest path
         # if length negligible, skip
         # else send left/right wheel speeds according to segment type
+        if selected_types is None:
+            planning_funcs = _PATH_TYPE_MAP.values()
+        else:
+            planning_funcs = [_PATH_TYPE_MAP[ptype] for ptype in selected_types]
+        dx = end_x
+        dy = end_y
+        d = hypot(dx, dy) * curvature
+
+        theta = _mod2pi(atan2(dy, dx))
+        alpha = _mod2pi(-theta)
+        beta = _mod2pi(end_yaw - theta)
+
+        best_cost = float("inf")
+        b_d1, b_d2, b_d3, b_mode = None, None, None, None
+
+        for planner in planning_funcs:
+            d1, d2, d3, mode = planner(alpha, beta, d)
+            if d1 is None:
+                continue
+
+            cost = (abs(d1) + abs(d2) + abs(d3))
+            if best_cost > cost:  # Select minimum length one.
+                b_d1, b_d2, b_d3, b_mode, best_cost = d1, d2, d3, mode, cost
+
+        lengths = [b_d1, b_d2, b_d3]
+
+        lengths = [length / curvature for length in lengths]
 
         pass
+
 
     # David
     def send_packet(self, id, left_speed, right_speed):
